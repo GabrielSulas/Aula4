0f1b857d103c7b7acf721c66edd735491dfd85b2
GabrielSulas <42851794+GabrielSulas@users.noreply.github.com>
2018-08-30 20:30:13 -0300

    Add files via upload

--- JavaApplication4.java
+++ JavaApplication4.java
@@ -7,60 +7,51 @@ public class JavaApplication4 {
     public static void main(String[] args) {
         Scanner ler = new Scanner (System.in);
         int mes;
-       
-        System.out.println("Deseja saber a partir de qual mes?");
+        
+        System.out.println("Deseja saber qual mes?");
         mes = ler.nextInt();
         
         switch (mes) {
-            
+         
             case 1:
-                System.out.println("Janeiro / Fevereiro / Março / Abril"
-                        + " / Maio / Junho / Julho / Agosto / Setembro"
-                        + " / Outubro / Novembro / Dezembro ");
+                System.out.println("Janeiro");
                 break;
                 case 2:
-                System.out.println("Fevereiro / Março / Abril"
-                        + " / Maio / Junho / Julho / Agosto / Setembro"
-                        + " / Outubro / Novembro / Dezembro ");
+                System.out.println("Fevereiro");
                 break;
                 case 3:
-                System.out.println("Março / Abril"
-                        + " / Maio / Junho / Julho / Agosto / Setembro"
-                        + " / Outubro / Novembro / Dezembro ");
+                System.out.println("Março");
                 break;
                 case 4:
-                System.out.println("Abril / Maio / Junho / Julho / Agosto / Setembro"
-                        + " / Outubro / Novembro / Dezembro");
+                System.out.println("Abril");
                 break;
                 case 5:
-                System.out.println("Maio / Junho / Julho / Agosto / Setembro"
-                        + " / Outubro / Novembro / Dezembro");
+                System.out.println("Maio");
                 break;
                 case 6:
-                System.out.println("Junho / Julho / Agosto / Setembro"
-                        + " / Outubro / Novembro / Dezembro");
+                System.out.println("Junho");
                 break;
                 case 7:
-                System.out.println("Julho / Agosto / Setembro"
-                        + " / Outubro / Novembro / Dezembro");
+                System.out.println("Julho");
                 break;
                 case 8:
-                System.out.println("Agosto / Setembro"
-                        + " / Outubro / Novembro / Dezembro");
+                System.out.println("Agosto");
                 break;
                 case 9:
-                System.out.println("Setembro / Outubro / Novembro / Dezembro");
+                System.out.println("Setembro");
                 break;
                 case 10:
-                System.out.println("Outubro / Novembro / Dezembro");
+                System.out.println("Outubro");
                 break;
                 case 11:
-                System.out.println("Novembro / Dezembro");
+                System.out.println("Novembro");
                 break;
                 case 12:
                 System.out.println("Dezembro");
-                break;
-             
+                break;                               
+                    }
+          if (mes > 12 || mes >= 0 ) {
+                        System.out.println("Mes invalido!");
         }
     }
 
